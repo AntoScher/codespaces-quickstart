e972ef316dfa444289460306f1baf463f24afbe5
AntoScher <141661447+AntoScher@users.noreply.github.com>
2025-03-29 08:14:50 +0000

    ниченг

--- main.py
+++ main.py
@@ -2,8 +2,15 @@ import os
 from dotenv import load_dotenv
 load_dotenv()
 
+telegram_token = os.getenv("TELEGRAM_BOT_TOKEN")
+if telegram_token:
+    print(f"Telegram Token: {telegram_token}")
+else:
+    print("Telegram Token not found!")
+
 rasa_license = os.getenv("RASA_PRO_LICENSE")
 if rasa_license:
     print("Лицензия успешно загружена!")
 else:
     print("Лицензия не найдена.")
+    
